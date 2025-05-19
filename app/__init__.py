fa1c2aaca655bdf2d58a7bbbc29097554230bbaf
Dua Baig <dbaig50@stuy.edu>
2025-05-19 10:06:27 -0400

    worked on making dicts

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,18 @@
 import csv
 
-with open('subject_gpa.csv', newline=None) as csvfile:
-    fieldnames = ["StudentID", "Attempted","Earned","English/ENL attempted","English/ENL earned",
+neededGPAInfo = ["StudentID", "Attempted","Earned","English/ENL attempted","English/ENL earned",
     "Social Studies attempted","Social Studies earned","Mathematics attempted","Mathematics earned", "Science attempted",
     "Science earned", "Foreign Language attempted", "Foreign Language earned", "Career & Tech Education attempted",
     "Career & Tech Education earned,Arts attempted","Arts earned","Health/Physical Education attempted", 
     "Health/Physical Education earned","Miscellaneous/Guidance attempted", "Miscellaneous/Guidance earned"]
-    studentGpas = csv.DictReader(csvfile, fieldnames = fieldnames)
-    for row in studentGpas:
+
+with open('subject_gpa.csv', newline='') as csvfile:
+    gpaRaw = csv.DictReader(csvfile)
+    for row in gpaRaw:
         print(row)
+        
+
 
 with open('ap_preferences.csv', newline='') as csvfile:
-    print("hi")
+    #fieldnames = ["student_id","rule_id","rule_name","courses_1","courses_2","courses_3","courses_4"]
+    studentPref = csv.DictReader(csvfile)
