4fbfe2c732b13e930248e08e3c77326de5a6c0df
Dua Baig <dbaig50@stuy.edu>
2025-05-23 11:47:35 -0400

    Filtered Data

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,29 @@
 import csv
+import json
 
-neededGPAInfo = ["StudentID", "Attempted","Earned","English/ENL attempted","English/ENL earned",
-    "Social Studies attempted","Social Studies earned","Mathematics attempted","Mathematics earned", "Science attempted",
-    "Science earned", "Foreign Language attempted", "Foreign Language earned", "Career & Tech Education attempted",
-    "Career & Tech Education earned,Arts attempted","Arts earned","Health/Physical Education attempted",
-    "Health/Physical Education earned","Miscellaneous/Guidance attempted", "Miscellaneous/Guidance earned"]
+neededGPAInfo = ["StudentID", "Class", "Attempted","Earned","English/ENL attempted","English/ENL earned",
+                 "Social Studies attempted","Social Studies earned","Mathematics attempted","Mathematics earned", "Science attempted",
+                 "Science earned", "Foreign Language attempted", "Foreign Language earned", "Career & Tech Education attempted",
+                 "Career & Tech Education earned","Arts attempted","Arts earned","Health/Physical Education attempted",
+                  "Health/Physical Education earned","Miscellaneous/Guidance attempted", "Miscellaneous/Guidance earned", "rule_id,rule_name,courses_1,courses_2,courses_3,courses_4,courses_"]
+
+neededPrefInfo = ["student_id", "ruleID", ""]
+
+filteredData = []
 
 with open('subject_gpa.csv', newline='') as csvfile:
+    #neededInfo = csvfile.readline().strip('\n')
+    #csvfile.seek(0)
     gpaRaw = csv.DictReader(csvfile)
     for row in gpaRaw:
-        print(row)
-
-
+        data = {i: row[i] for i in neededGPAInfo}
+        filteredData.append(data)
+    print(filteredData)
 
 with open('ap_preferences.csv', newline='') as csvfile:
-    #fieldnames = ["student_id","rule_id","rule_name","courses_1","courses_2","courses_3","courses_4"]
     studentPref = csv.DictReader(csvfile)
+
+filename = 'data.json'
+
+with open(filename, 'w') as file:
+    json.dump(filteredData, file, indent=4)
