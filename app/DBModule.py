7ecc116eb43d992b5435bd9b83e0b72e786730f7
Dua Baig <dbaig50@stuy.edu>
2025-06-02 11:45:46 -0400

    verified population of student table

--- app/DBModule.py
+++ app/DBModule.py
@@ -1,6 +1,6 @@
-import sqlite
+import sqlite3
 
-dbFile = "studentData"
+dbFile = "studentData.db"
 
 def initDB():
     db = sqlite3.connect(dbFile)
@@ -14,7 +14,8 @@ def initDB():
               scienceGPA DOUBLE,
               socialStudiesGPA DOUBLE,
               foreignLangGPA DOUBLE,
-              artMusicGPA DOUBLE,
+              artMusicGPA DOUBLE
+              )
               '''
               )
     c.execute('''
@@ -26,15 +27,17 @@ def initDB():
               courseTwoID TEXT,
               courseThreeID TEXT,
               courseFourID TEXT,
-              FOREIGN KEY(studentID) REFERENCES students(studentID)
+              FOREIGN KEY (studentID) REFERENCES students(studentID)
+              )
               '''
           )
     c.execute('''
               CREATE TABLE IF NOT EXISTS studentAP(
-              studentID INTEGER
-              courseID TEXT
-              status TEXT
-              FOREIGN KEY(studentID) REFERENCES students(studentID)
+              studentID INTEGER,
+              courseID TEXT,
+              status TEXT,
+              FOREIGN KEY (studentID) REFERENCES students(studentID)
+              )
               '''
               )
     c.execute('''
@@ -43,17 +46,19 @@ def initDB():
               courseName TEXT,
               totalSeats INTEGER,
               seatsTaken INTEGER,
-              seatsRemaining INTEGER,
+              seatsRemaining INTEGER
+              )
               '''
               )
-              
+
     db.commit()
     db.close()
 
 def addGPA(id, eng, math, science, ss, foreignLang, artMusic):
     db = sqlite3.connect(dbFile)
     c = db.cursor()
-    c.execute("INSERT INTO students(studentID, englishGPA, mathGPA, scienceGPA, socialStudiesGPA, foreignLangGPA, artMusicGPA) VALUES (?, ?, ?, ?, ?, ?)", (id, eng, math, science, ss, artMusic))
+    c.execute("INSERT INTO students(studentID, englishGPA, mathGPA, scienceGPA, socialStudiesGPA, foreignLangGPA, artMusicGPA) VALUES (?, ?, ?, ?, ?, ?, ?)", (id, eng, math, science, ss, foreignLang, artMusic))
+    #print("row added")
     db.commit()
     db.close()
 
@@ -71,9 +76,23 @@ def addStudentAP(id, courseID, status):
     db.commit()
     db.close()
 
-def addApCourses(id, name, totalSeats, seatsTaken, seatsRemaining):
+def addApCourses(id, name, totalSeats, seatsTaken, seatsRemaining): # use only once
     db = sqlite3.connect(dbFile)
     c = db.cursor()
     c.execute("INSERT INTO apCourses(courseID, courseName, totalSeats, seatsTaken, seatsRemaining) VALUES (?, ?, ?, ?, ?)", (id, name, totalSeats, seatsTaken, seatsRemaining))
     db.commit()
+    db.close()
+
+# WIP
+def viewTable():
+    db = sqlite3.connect(dbFile)
+    c = db.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    tables = [row[0] for row in c.fetchall()]
+    for table in tables:
+        print(f"\n{table}")
+        c.execute(f"SELECT * FROM {table}")
+        for row in c.fetchall():
+            print(row)
+
     db.close()
--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,18 @@
+import sqlite3
 import csv
-import json
+import DBModule as db 
 
-neededGPAInfo = ["StudentID", "Class", "Attempted","Earned","English/ENL attempted","English/ENL earned",
-                 "Social Studies attempted","Social Studies earned","Mathematics attempted","Mathematics earned", "Science attempted",
-                 "Science earned", "Foreign Language attempted", "Foreign Language earned", "Career & Tech Education attempted",
-                 "Career & Tech Education earned","Arts attempted","Arts earned","Health/Physical Education attempted",
-                  "Health/Physical Education earned","Miscellaneous/Guidance attempted", "Miscellaneous/Guidance earned", "rule_id,rule_name,courses_1,courses_2,courses_3,courses_4,courses_"]
-
-neededPrefInfo = ["student_id", "ruleID", ""]
-
-filteredData = []
+db.initDB()
 
 with open('subject_gpa.csv', newline='') as csvfile:
-    #neededInfo = csvfile.readline().strip('\n')
-    #csvfile.seek(0)
     gpaRaw = csv.DictReader(csvfile)
     for row in gpaRaw:
-        data = {i: row[i] for i in neededGPAInfo}
-        filteredData.append(data)
-    print(filteredData)
+        db.addGPA(row.get("StudentID"), row.get("English/ENL Avg"), row.get("Mathematics Avg"), row.get("Science Avg"),
+        row.get("Social Studies Avg"), row.get("Foreign Language Avg"), row.get("Arts Avg"))
+
 
 with open('ap_preferences.csv', newline='') as csvfile:
+    #fieldnames = ["student_id","rule_id","rule_name","courses_1","courses_2","courses_3","courses_4"]
     studentPref = csv.DictReader(csvfile)
 
-filename = 'data.json'
-
-with open(filename, 'w') as file:
-    json.dump(filteredData, file, indent=4)
+db.viewTable()
