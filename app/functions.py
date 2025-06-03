d3535d0eb0943ea50c09958cb5d9c62bd8f6f8c2
Naf (Nafiyu Murtaza) <nmurtaza50@stuy.edu>
2025-06-03 11:47:45 -0400

    a

--- app/functions.py
+++ app/functions.py
@@ -12,7 +12,7 @@ c = db.cursor()
 # Helper Functions
 
 # Clean the bloat from student_prefs and return a list of the student's actual preferred courses. The first parameter is how many AP courses they have selcted.
-def cleanStudentPrefs(id):
+def cleanStudentPrefs(id): #look thru all lines: 
     c.execute(f"SELECT {id}")
 
 # Best Come First
