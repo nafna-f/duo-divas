2e59e973ea73cbcf2afb7a7b56414ebe8779d01e
Naf (Nafiyu Murtaza) <nmurtaza50@stuy.edu>
2025-06-04 11:32:21 -0400

    H

--- app/functions.py
+++ app/functions.py
@@ -3,18 +3,17 @@
 import sqlite3
 import os
 
-# DB file
-
-dbFile = "studentData.db"
-db = sqlite3.connect(dbFIle)
-c = db.cursor()
-
 # Helper Functions
 
 # Clean the bloat from student_prefs and return a list of the student's actual preferred courses. The first parameter is how many AP courses they have selcted.
-def cleanStudentPrefs(id): #look thru all lines: 
-    c.execute(f"SELECT {id}")
-
+def cleanStudentPrefs(id):
+    dbFile = "studentData.db"
+    db = sqlite3.connect(dbFIle)
+    c = db.cursor()
+    c.execute(f"SELECT * FROM courseRanking WHERE studentID = '{id}'")
+    for data in cursor.fetchall():
+        print(data)
+    db.close()
 # Best Come First
 
 def bestComeFirst():
